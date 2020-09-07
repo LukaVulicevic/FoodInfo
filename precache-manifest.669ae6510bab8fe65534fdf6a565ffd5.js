cda195cc8e7a3f57986a80a4af19f11bc5200453
LukaVulicevic <67145667+LukaVulicevic@users.noreply.github.com>
2020-09-07 11:15:55 +0200

    Updates

---
+++ precache-manifest.669ae6510bab8fe65534fdf6a565ffd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226065a2b1051bb570d92698cba55481",
+    "url": "/FoodInfo/index.html"
+  },
+  {
+    "revision": "f3b4ef9549e31047f6e7",
+    "url": "/FoodInfo/static/css/main.7dd63364.chunk.css"
+  },
+  {
+    "revision": "fcd4a560e38c9149d8b5",
+    "url": "/FoodInfo/static/js/2.e6907142.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FoodInfo/static/js/2.e6907142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b4ef9549e31047f6e7",
+    "url": "/FoodInfo/static/js/main.647eea98.chunk.js"
+  },
+  {
+    "revision": "1d70b409403ed2a31547",
+    "url": "/FoodInfo/static/js/runtime-main.5def4d08.js"
+  },
+  {
+    "revision": "fbad0d49ac48932b0dc08cf398741c5e",
+    "url": "/FoodInfo/static/media/knife-salad-darken.png.fbad0d49.png"
+  }
+]);
